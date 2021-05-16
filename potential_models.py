672e0aaa2379c1899d7142fce4b11bd4de8b961e
Sam May <samrmay@sas.upenn.edu>
2021-05-16 01:17:17 -0400

    add function to plot adiabatic potentials

--- potential_models.py
+++ potential_models.py
@@ -1,5 +1,6 @@
 import math
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class Simple_Avoided_Crossing:
@@ -31,3 +32,25 @@ class Simple_Avoided_Crossing:
         dV12 = dV21 = -2*self.C*self.D*x*math.exp(-self.D*(x**2))
 
         return np.asarray([[dV11, dV12], [dV21, dV22]])
+
+# Plots adiabatic potential from diabatic representation
+
+
+def plot_adiabatic_potential(model, x0, x1):
+    x_linspace = np.linspace(x0, x1)
+    adiabatic_1 = np.zeros(len(x_linspace))
+    adiabatic_2 = np.zeros(len(x_linspace))
+    for i in range(len(x_linspace)):
+        x = x_linspace[i]
+
+        diabatic = model.V(x)
+        adiabatic = np.diag(diabatic)
+        adiabatic_1[i] = adiabatic[0]
+        adiabatic_2[i] = adiabatic[1]
+
+    plt.plot(x_linspace, adiabatic_1)
+    plt.plot(x_linspace, adiabatic_2)
+    plt.show()
+
+
+plot_adiabatic_potential(Simple_Avoided_Crossing(), -10, 10)
