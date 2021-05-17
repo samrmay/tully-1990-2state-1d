f774860bbd12e5d85333382d466def2df08c672c
Sam May <samrmay@sas.upenn.edu>
2021-05-16 22:12:47 -0400

    Add canned PES settings

--- potential_models_test.py
+++ potential_models_test.py
@@ -1,4 +1,19 @@
 import potential_models as models
 
-models.plot_adiabatic_potential(
-    models.Extended_Coupling_With_Reflection(), -10, 10)
+
+def simple():
+    models.plot_adiabatic_potential(
+        models.Simple_Avoided_Crossing(), -10, 10, 200, 1/50)
+
+
+def double():
+    models.plot_adiabatic_potential(
+        models.Double_Avoided_Crossing(), -10, 10, 200, 1/12)
+
+
+def extended():
+    models.plot_adiabatic_potential(
+        models.Extended_Coupling_With_Reflection(), -10, 10, 200, 1)
+
+
+simple()
